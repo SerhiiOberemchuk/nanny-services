7f9d3bfe70f48c9c7bad34a559deb1fc78f26b72
SerhiiOberemchuk <SerhiiOberemchuk@users.noreply.github.com>
2024-05-29 21:11:56 +0000

    deploy: f12e6c288c6b85a428e9072e54861d278261843a

---
+++ assets/FavoritesPage-DIfovDNy.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-C3Myh7BU.js";function e(){return r.jsx("div",{children:"Favorites"})}export{e as default};
--- assets/FavoritesPage-DRVyOSx_.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CHQ7m-A8.js";function e(){return r.jsx("div",{children:"Favorites"})}export{e as default};
