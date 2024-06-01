143d9d7266f6645bd6965d1f6b7bb704f681183e
SerhiiOberemchuk <SerhiiOberemchuk@users.noreply.github.com>
2024-06-01 10:11:23 +0000

    deploy: 0fced1b931756fbd19dbe6708f85b6a157040a78

--- assets/FavoritesPage-7wbNaymW.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-nKPN-MOR.js";function e(){return r.jsx("div",{children:"Favorites"})}export{e as default};
---
+++ assets/FavoritesPage-D64rMG5V.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-Ce8cI25W.js";function e(){return r.jsx("div",{children:"Favorites"})}export{e as default};
